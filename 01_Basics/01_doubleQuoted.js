8ae4949120c2abf483040b79d941f0ffd41ff132
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-07 23:59:56 +0500

    Update 01_doubleQuoted.js

--- 01_Basics/01_doubleQuoted.js
+++ 01_Basics/01_doubleQuoted.js
@@ -1,3 +1,9 @@
-const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+Output:
+FirstLine
+    \SecondLine
+ThirdLine
