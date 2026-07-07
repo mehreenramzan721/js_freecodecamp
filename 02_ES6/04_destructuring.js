3f7e3f3499dd98dca0557481722b4fb01e1fe09a
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-07 16:11:32 +0000

    destruct

--- 02_ES6/04_destructuring.js
+++ 02_ES6/04_destructuring.js
@@ -38,3 +38,15 @@ function removeFirstTwo(list) {
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sourceWithoutFirstTwo = removeFirstTwo(source);
 // now we will get first two element removed from the array and the answer will be the remainings
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({ max, min }) => (max + min) / 2;// Only change code above this line
