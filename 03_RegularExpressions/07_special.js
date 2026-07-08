92421a053dfb590453fedcba9d548e855a99cae2
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 11:44:45 +0000

    up

---
+++ 03_RegularExpressions/07_special.js
@@ -0,0 +1,17 @@
+// let ohStr = "Ohhh no";
+// let ohRegex = /Oh{3,6} no/; // Change this line
+// let result = ohRegex.test(ohStr);
+
+
+// let haStr = "Hazzzzah";
+// let haRegex = /Haz{4,}ah/; // Change this line
+// let result = haRegex.test(haStr);
+
+
+// let timStr = "Timmmmber";
+// let timRegex = /Tim{4}ber/; // Change this line
+// let result = timRegex.test(timStr);
+
+// let favWord = "favorite";
+// let favRegex = /favou?rite/; // Change this line
+// let result = favRegex.test(favWord);
