0a8d1ac8c9a99726053e189a5bfee4059810a810
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 09:54:26 +0000

    match

---
+++ 03_RegularExpressions/03_wildCard.js
@@ -0,0 +1,18 @@
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /.un/; // Change this line
+// let result = unRegex.test(exampleStr);
+
+
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/ig; // Change this line
+// let result = quoteSample.match(vowelRegex); // Change this line
+
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
