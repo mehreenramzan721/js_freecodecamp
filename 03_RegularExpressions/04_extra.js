e7eb598d6b2ae8e445858ef4127643de892945f2
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 10:13:22 +0000

    done

---
+++ 03_RegularExpressions/04_extra.js
@@ -0,0 +1,14 @@
+// let quoteSample = "3 blind mice.";
+// let myRegex = /[^aeiou0-9]/gi; // Change this line
+// let result =quoteSample.match(myRegex); // Change this line
+
+// let difficultSpelling = "Mississippi";
+// let myRegex = /s+/g; // Change this line
+// let result = difficultSpelling.match(myRegex);
+
+const chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
