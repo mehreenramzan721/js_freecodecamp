a085f4c365f90eed91280508a1eccf83cdd62f4e
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 11:18:52 +0000

    update

--- 03_RegularExpressions/05_lazy.js
+++ 03_RegularExpressions/05_lazy.js
@@ -24,6 +24,11 @@
 
 
 
+// let movieName = "2001: A Space Odyssey";
+// let numRegex = /\d/g; // Change this line
+// let result = movieName.match(numRegex).length;
+
+
 let movieName = "2001: A Space Odyssey";
-let numRegex = /\d/g; // Change this line
-let result = movieName.match(numRegex).length;
+let noNumRegex = /\D/g; // Change this line
+let result = movieName.match(noNumRegex).length;
