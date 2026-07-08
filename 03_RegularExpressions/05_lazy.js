e1cd5737a007a71a7c24a773bad9fd8b7ddd65c0
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 11:13:46 +0000

    update

--- 03_RegularExpressions/05_lazy.js
+++ 03_RegularExpressions/05_lazy.js
@@ -8,6 +8,11 @@
 // let calRegex = /^Cal/; // Change this line
 // let result = calRegex.test(rickyAndCal);
 
-let caboose = "The last car on a train is the caboose";
-let lastRegex = /caboose$/; // Change this line
-let result = lastRegex.test(caboose);
+// let caboose = "The last car on a train is the caboose";
+// let lastRegex = /caboose$/; // Change this line
+// let result = lastRegex.test(caboose);
+
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
