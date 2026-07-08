3c3c27000db02ee146c8f9a83c5b3b3e5ae3e9e5
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 10:32:43 +0000

    update

--- 03_RegularExpressions/05_lazy.js
+++ 03_RegularExpressions/05_lazy.js
@@ -4,6 +4,10 @@
 
 // let reCriminals = /C+/; // Change this line
 
-let rickyAndCal = "Cal and Ricky both like racing.";
-let calRegex = /^Cal/; // Change this line
-let result = calRegex.test(rickyAndCal);
+// let rickyAndCal = "Cal and Ricky both like racing.";
+// let calRegex = /^Cal/; // Change this line
+// let result = calRegex.test(rickyAndCal);
+
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
