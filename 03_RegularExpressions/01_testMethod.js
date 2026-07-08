fb1eac0f0a791635ffd29b39b42c5d62a874f2e1
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 09:31:55 +0000

    test practice

---
+++ 03_RegularExpressions/01_testMethod.js
@@ -0,0 +1,12 @@
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex.test(myString); // Change this line 
+
+
+// let petString = "James has a pet cat.";
+// let petRegex = /cat| dog| bird| fish/; // Change this line
+// let result = petRegex.test(petString);
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
