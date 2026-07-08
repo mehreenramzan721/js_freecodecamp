a88a3af18962a9df6e2185bdba91cab42778c830
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 12:15:58 +0000

    completed

---
+++ 03_RegularExpressions/08_end.js
@@ -0,0 +1,23 @@
+// let sampleWord = "astronaut";
+// let pwRegex = /(?=\w{6,})(?=\D*\d\d)/; // Change this line
+// let result = pwRegex.test(sampleWord);
+
+
+// let myString = "Eleanor Roosevelt";
+// let myRegex = /(Franklin|Eleanor)\s(.*\s)?Roosevelt/; // Change this line
+// let result = myRegex.test(myString); // Change this line
+// // After passing the challenge experiment with myString and see how the grouping works
+
+
+// let repeatNum = "42 42 42";
+// let reRegex = /^(\d+) \1 \1$/;
+// // Change this line
+// let result = reRegex.test(repeatNum);
+
+
+// let str = "one two three";
+// let fixRegex = /(\w+) (\w+) (\w+)/;
+// let replaceText = "$3 $2 $1"; 
+// let result = str.replace(fixRegex, replaceText);
+
+
