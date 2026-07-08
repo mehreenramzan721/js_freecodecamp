a096c8e3f47093314466575ad9ba50a4d1e68099
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-08 11:16:57 +0000

    update

--- 03_RegularExpressions/05_lazy.js
+++ 03_RegularExpressions/05_lazy.js
@@ -18,6 +18,12 @@
 // let result = quoteSample.match(alphabetRegexV2).length;
 
 
-let quoteSample = "The five boxing wizards jump quickly.";
-let nonAlphabetRegex = /\W/g; // Change this line
-let result = quoteSample.match(nonAlphabetRegex).length;
+// let quoteSample = "The five boxing wizards jump quickly.";
+// let nonAlphabetRegex = /\W/g; // Change this line
+// let result = quoteSample.match(nonAlphabetRegex).length;
+
+
+
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length;
