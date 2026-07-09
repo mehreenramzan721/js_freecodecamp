8c26f6a9faf7edbdcba857a623f26afcfe64603a
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-09 14:48:46 +0000

    inheri

---
+++ 06_OOP/01_Object.js
@@ -0,0 +1,44 @@
+// function Dog(name) {
+//   this.name = name;
+// }
+
+// // Only change code below this line
+// function joinDogFraternity(candidate) {
+//   if(candidate.constructor == Dog){
+//     return true;
+//   }
+//   return false;
+// }
+
+
+// function Dog(name) {
+//   this.name = name;
+// }
+
+// Dog.prototype = {
+//   // Only change code below this line
+//   numLegs: 2,
+//   eat: function(){
+//     console.log("yssa");
+//   },
+//   describe: function(){
+//     console.log("")
+//   }
+// };
+
+
+// function Dog(name) {
+//   this.name = name;
+// }
+
+// // Only change code below this line
+// Dog.prototype = {
+//   constructor: Dog,
+//   numLegs: 4,
+//   eat: function() {
+//     console.log("nom nom nom");
+//   },
+//   describe: function() {
+//     console.log("My name is " + this.name);
+//   }
+// };
---
+++ 06_OOP/02_inher.js
@@ -0,0 +1,50 @@
+// function Animal() { }
+
+// Animal.prototype = {
+//   constructor: Animal,
+//   eat: function() {
+//     console.log("nom nom nom");
+//   }
+// };
+
+// // Only change code below this line
+
+// let duck = Object.create(Animal.prototype); // Change this line
+// let beagle=Object.create(Animal.prototype); // Change this line
+
+
+// function Animal() { }
+
+// Animal.prototype = {
+//   constructor: Animal,
+//   eat: function() {
+//     console.log("nom nom nom");
+//   }
+// };
+
+// function Dog() { }
+
+// // Only change code below this line
+
+// Dog.prototype = Object.create(Animal.prototype);
+
+// let beagle = new Dog();
+
+
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+
+let duck = new Bird();
+let beagle = new Dog();
+duck.constructor;
+beagle.constructor;
