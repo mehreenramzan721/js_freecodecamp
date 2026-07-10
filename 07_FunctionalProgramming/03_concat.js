5848380d59a25568b2eeac494ee9c24dad6ae7f6
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-10 13:01:01 +0000

    concat

---
+++ 07_FunctionalProgramming/03_concat.js
@@ -0,0 +1,22 @@
+// function nonMutatingConcat(original, attach) {
+//   // Only change code below this line
+//  return original.concat(attach);
+
+//   // Only change code above this line
+// }
+
+// const first = [1, 2, 3];
+// const second = [4, 5];
+// nonMutatingConcat(first, second);
+
+
+// function nonMutatingPush(original, newItem) {
+//   // Only change code below this line
+//   return original.concat(newItem);
+
+//   // Only change code above this line
+// }
+
+// const first = [1, 2, 3];
+// const second = [4, 5];
+// nonMutatingPush(first, second);
